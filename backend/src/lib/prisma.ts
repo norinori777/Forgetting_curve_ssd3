3342fc6d65c3dd742711b73671b9ca139d755768
norinori777 <norinori6791@gmail.com>
2026-04-11 19:20:05 +0900

    型チェックのエラーを修正

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/index";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const databaseUrl = process.env.DATABASE_URL;
